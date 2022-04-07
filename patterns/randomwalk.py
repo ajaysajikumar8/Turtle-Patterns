18488844c8ee9b41f2730ad2a92782f13d7776cf
Ajay Sajikumar <71449787+ajaysajikumar8@users.noreply.github.com>
2022-04-07 21:05:06 +0530

    Update randomwalk.py

--- patterns/randomwalk.py
+++ patterns/randomwalk.py
@@ -1,14 +1,23 @@
-from turtle import Turtle, Screen
-import random
 
+import random
+from turtle import *
+import turtle
 
 new_tur = Turtle()
 new_tur.pensize(2)
 new_tur.speed("fast")
+turtle.colormode(255)
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    return (r,g,b)
 
 direction = [0,90,180,270]
 
 for _ in range(500):
+    new_tur.color(random_color())
     new_tur.forward(50)
     new_tur.setheading(random.choice(direction))
     
